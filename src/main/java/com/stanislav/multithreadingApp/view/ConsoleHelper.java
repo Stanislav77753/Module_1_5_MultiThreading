91b4f99856d5f0d78fb961f2edbd580fa2924839
Stas <maikydyk@mail.ru@git config --global user.email maikydyk@mail.ru>
2018-08-19 19:32:57 +0300

    была опечатка

--- src/main/java/com/stanislav/multithreadingApp/view/ConsoleHelper.java
+++ src/main/java/com/stanislav/multithreadingApp/view/ConsoleHelper.java
@@ -15,7 +15,7 @@ public class ConsoleHelper {
         while (!command.equals("close programm")){
             System.out.println("You are in main menu");
             System.out.println("You need enter path to directory");
-            System.out.println("For to close programm enter cpmmand \"close programm\"");
+            System.out.println("For to close programm enter command \"close programm\"");
             command = scanner.nextLine().toLowerCase().trim();
             if(!command.equals("close programm")){
                 File directory = new File(command);
